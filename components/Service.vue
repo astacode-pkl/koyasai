3959a892c220417f31b61b26d56ab3a80a3f7eac
tasyafbriani <tasyanurfebrianiputri@gmail.com>
2025-02-24 11:28:43 -0800

    firs

---
+++ components/Service.vue
@@ -0,0 +1,98 @@
+<template>
+   <div class="max-w-6xl mx-auto px-4 py-16">
+    <h2 class="text-orange-500 text-2xl font-semibold mb-6 text-center sm:text-left" data-aos="fade-right" data-aos-duration="3000"  >
+      Services</h2>
+    <hr class="border-t border-orange-500 w-24 mb-6 mx-auto sm:mx-0" data-aos="fade-left" data-aos-duration="3000"    />
+
+    <!-- Grid Section -->
+    <div class="font-sans max-w-6xl mx-auto px-4 py-16">
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6 text-center">
+
+      <!-- First Card -->
+        <div data-aos="fade-up" data-aos-duration="3000" 
+     class="flex flex-col justify-center p-6 text-center border rounded-lg transition-all duration-300 hover:sweep-to-bottom  hover:bg-orange-600 hover:transform hover:scale-105 hover:shadow-lg hover:text-white group w-full h-64">
+        <svg class="w-8 h-8 text-orange-500 mb-4 group-hover:text-white"  aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m7.171 12.906-2.153 6.411 2.672-.89 1.568 2.34 1.825-5.183m5.73-2.678 2.154 6.411-2.673-.89-1.568 2.34-1.825-5.183M9.165 4.3c.58.068 1.153-.17 1.515-.628a1.681 1.681 0 0 1 2.64 0 1.68 1.68 0 0 0 1.515.628 1.681 1.681 0 0 1 1.866 1.866c-.068.58.17 1.154.628 1.516a1.681 1.681 0 0 1 0 2.639 1.682 1.682 0 0 0-.628 1.515 1.681 1.681 0 0 1-1.866 1.866 1.681 1.681 0 0 0-1.516.628 1.681 1.681 0 0 1-2.639 0 1.681 1.681 0 0 0-1.515-.628 1.681 1.681 0 0 1-1.867-1.866 1.681 1.681 0 0 0-.627-1.515 1.681 1.681 0 0 1 0-2.64c.458-.361.696-.935.627-1.515A1.681 1.681 0 0 1 9.165 4.3ZM14 9a2 2 0 1 1-4 0 2 2 0 0 1 4 0Z"/>
+</svg>
+        <h2 class="text-orange-500 text-xl font-semibold mb-4 group-hover:text-white">
+    Assured Excellence
+  </h2>
+        <span class="text-sm">Exacting standards and ensure paralleled quality</span>
+      </div>
+
+      <!-- Second Card -->
+      <div data-aos="fade-up" data-aos-duration="2500"
+        class="flex flex-col justify-center p-6 text-center border rounded-lg transition-all duration-300 hover:bg-orange-600 hover:transform hover:scale-105 hover:shadow-lg hover:text-white group w-full h-64">
+       
+        <svg class="w-8 h-8 text-orange-500 mb-4 group-hover:text-white" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 7h18M3 7l3 14h12l3-14M8 10h8v7H8z"/>
+</svg>
+        <h2 class="text-orange-500 text-xl font-semibold mb-4 group-hover:text-white">
+          Abundant Selection
+        </h2>
+        <span class="text-sm">Handpick from a multi varieties of the freshest fruits.</span>
+      </div>
+
+      <!-- Third Card -->
+      <div data-aos="fade-up" data-aos-duration="2000"
+        class="flex flex-col justify-center p-6 text-center border rounded-lg transition-all duration-300 hover:bg-orange-600 hover:transform hover:scale-105 hover:shadow-lg hover:text-white group w-full h-64">
+        <svg class="w-8 h-8 text-orange-500 mb-4 group-hover:text-white" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12l5 5L20 7" />
+  <circle cx="12" cy="12" r="9" />
+</svg>
+        <h2 class="text-orange-500 text-xl font-semibold mb-4 group-hover:text-white">
+          Punctual Perfection
+        </h2>
+        <span class="text-sm">Timely deliveries guaranteed by our dedicated logistics team.</span>
+      </div>
+
+      <!-- Fourth Card -->
+      <div data-aos="fade-up" data-aos-duration="1000"
+        class="flex flex-col justify-center p-6 text-center border rounded-lg transition-all duration-300 hover:bg-orange-600 hover:transform hover:scale-105 hover:shadow-lg hover:text-white group w-full h-64">
+        <svg class="w-8 h-8 text-orange-500 mb-4 group-hover:text-white" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 17l-5 3 1-6-4-4h5L12 2l2 5h5l-4 4 1 6-5-3z" />
+</svg>
+        <h2 class="text-orange-500 text-xl font-semibold mb-4 group-hover:text-white">
+          Decades of Mastery
+        </h2>
+        <span class="text-sm">Rely on our more than 40 years of expert market knowledge and experience.</span>
+      </div>
+    </div>
+  </div>
+  </div>
+</template>
+
+<style scoped>
+.group {
+  position: relative;
+  overflow: hidden;
+  transition: all 0.3s ease;
+}
+
+.group::before {
+  content: '';
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  height: 100%;
+  background-color: rgba(249, 115, 22, 0.192); 
+  transform: translateY(-100%);
+  transition: transform 0.8s ease-in-out;
+}
+
+.group:hover::before {
+  transform: translateY(0);
+  
+}
+
+.group:hover {
+  transform: scale(1.05);
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.2);
+  color: white;
+}
+</style>
+
+
+
+
